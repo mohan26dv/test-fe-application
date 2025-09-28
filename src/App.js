1ea8d9691ddd3f7ba7c3544fd3369eb72c14be5c
mohan kumar <mohan26.dv@gmail.com>
2025-09-28 15:40:43 +0530

    Added updated code

---
+++ src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Hello DigitalOcean 👋</h1>
+      <p>This is a very basic React app deployed on App Platform.</p>
+    </div>
+  );
+}
+
+export default App;
